aeadeb38f8aecd7ce65d06324b29c8a187c7e810
Ash <ash.woodall@gmail.com>
2016-10-12 11:08:48 -0500

    file var name change

--- src/client/views/Signup/Signup.js
+++ src/client/views/Signup/Signup.js
@@ -3,8 +3,9 @@ import React from 'react'
 
 import Auth from 'modules/Auth/Auth'
 
-const Signup = ({ register }) => (
-	<Auth type='register' submit={ register } />
-)
+const Signup = ({ register, registered }) => {
+	console.log(`is user register? ${registered}`)
+	return (<Auth type='register' submit={ register } />)
+}
 
 export default Signup
