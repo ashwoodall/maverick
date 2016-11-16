42c3ba2e0b8d97307de21a55eb43f60ab50598e0
Ash <ash.woodall@gmail.com>
2016-11-16 17:39:43 -0600

    Account settings

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -9,27 +9,41 @@ import Container from 'components/Container/Container'
 
 // Modules
 import ProfileEditor from 'modules/ProfileEditor/ProfileEditorContainer'
+import AccountSettings from 'modules/AccountSettings/AccountSettings'
 
 // Theme
 import theme from './Profile.scss'
 
-const Profile = () => (
-  <div className={ theme.profile } data-oh-hi='profile-view'>
-    <div className={ theme.toolbar } />
-    <Container>
-      <Tabs inverse>
-        <Tab className={ theme.tab } theme={ theme } label='Profile'>
-          <ProfileEditor />
-        </Tab>
-        <Tab label='References'>
-          <div>Hello world</div>
-        </Tab>
-        <Tab label='Account Settings'>
-          <div>Hello world</div>
-        </Tab>
-      </Tabs>
-    </Container>
-  </div>
-)
+class Profile extends React.Component {
+  state = {
+    inverseIndex: 1
+  };
+
+  handleInverseTabChange = (index) => {
+    this.setState({inverseIndex: index});
+  };
+
+  render() {
+    return (
+      <div className={ theme.profile } data-oh-hi='profile-view'>
+        <div className={ theme.toolbar } />
+        <Container> 
+          <Tabs index={this.state.inverseIndex} onChange={this.handleInverseTabChange} inverse>
+            <Tab className={ theme.tab } theme={ theme } label='Profile'>
+              <ProfileEditor />
+            </Tab>
+            <Tab label='References'>
+              <div>Hello world</div>
+            </Tab>
+            <Tab label='Account Settings'>
+              <AccountSettings />
+            </Tab>
+          </Tabs>
+        </Container>
+      </div>
+    )
+  }
+}
+
 
 export default Profile
